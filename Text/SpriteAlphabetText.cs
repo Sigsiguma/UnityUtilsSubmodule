918639be36df6a1a190cc6879d91661bc00e5507
Siguma <you8jun5@gmail.com>
2017-04-06 19:31:03 +0900

    TextをSpriteで表示できるような機能追加

---
+++ Text/SpriteAlphabetText.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+
+namespace utility.text {
+    public class SpriteAlphabetText : MonoBehaviour {
+
+        [SerializeField]
+        private Sprite[] alphabets_;
+
+        [SerializeField]
+        private StringReactiveProperty inputText_;
+
+        [SerializeField]
+        private AlphabetType type_;
+
+        [SerializeField]
+        private Vector2 textSize_;
+
+        private void Start() {
+            inputText_.Subscribe(WriteAlphabet);
+        }
+
+        private void WriteAlphabet(string word) {
+            var drawPos = Vector2.zero;
+
+            ResetWord();
+            foreach (var c in word) {
+                var index = (type_ == AlphabetType.Upper) ? c - 'A' : c - 'a';
+
+                if (index < alphabets_.Length) {
+                    CreateAlphabetImage(index, drawPos);
+                    drawPos.x += textSize_.x;
+                }
+            }
+        }
+
+        private void CreateAlphabetImage(int index, Vector2 drawPos) {
+            var obj = new GameObject("Word");
+            var renderer = obj.AddComponent<Image>();
+            renderer.sprite = alphabets_[index];
+            renderer.rectTransform.sizeDelta = textSize_;
+
+            obj.transform.position = drawPos;
+            obj.transform.SetParent(transform, false);
+        }
+
+        private void ResetWord() {
+            foreach (Transform child in gameObject.transform) {
+                Destroy(child.gameObject);
+            }
+        }
+
+        private enum AlphabetType {
+            Upper,
+            Lower
+        }
+    }
+}
---
+++ Text/SpriteDigitNumber.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+namespace utility.text {
+    public class SpriteDigitNumber : MonoBehaviour {
+
+        public StringReactiveProperty inputDigit_;
+
+        [SerializeField]
+        private Sprite[] digits_;
+
+        [SerializeField]
+        private Vector2 digitSize_;
+
+        private void Start() {
+            inputDigit_.Subscribe(WriteDigits);
+        }
+
+        private void WriteDigits(string digits) {
+            var drawPos = Vector2.zero;
+
+            ResetWord();
+            foreach (var digit in digits) {
+                var index = digit - '0';
+
+                if (index < digits_.Length) {
+                    CreateAlphabetImage(index, drawPos);
+                    drawPos.x += digitSize_.x;
+                }
+            }
+
+        }
+
+        private void CreateAlphabetImage(int index, Vector2 drawPos) {
+            var obj = new GameObject("Number");
+            var renderer = obj.AddComponent<Image>();
+            renderer.sprite = digits_[index];
+            renderer.rectTransform.sizeDelta = digitSize_;
+
+            obj.transform.position = drawPos;
+            obj.transform.SetParent(transform, false);
+        }
+
+        private void ResetWord() {
+            foreach (Transform child in gameObject.transform) {
+                Destroy(child.gameObject);
+            }
+        }
+
+    }
+}

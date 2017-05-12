27b8fdffcae9dbe91aec5f085806c04c3a4674e0
Siguma <you8jun5@gmail.com>
2017-05-12 20:22:30 +0900

    TextAlphaの追加(DOTween必須)

---
+++ Others/TextAlpha.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace utility.others {
+    public class TextAlpha : MonoBehaviour {
+
+        private Text clickText;
+
+        private void Awake() {
+            clickText = GetComponent<Text>();
+        }
+
+        private void Start() {
+            clickText = GetComponent<Text>();
+            DOTween.ToAlpha(() => clickText.color, color => clickText.color = color, 0.1f, 1.0f).SetLoops(-1, LoopType.Yoyo);
+        }
+
+    }
+}

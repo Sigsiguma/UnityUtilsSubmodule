9c725b859af99e3191651efb93baa234a3db8c68
siguma <you8jun5@gmail.com>
2017-07-22 23:35:52 +0900

    Editor時のみ表示するログの作成

---
+++ Log/DebugLog.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace utility.log {
+    public static class DebugLog {
+
+        [Conditional("UNITY_EDITOR")]
+        public static void Log (object log) {
+            UnityEngine.Debug.Log(log);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void TextLog (string text) {
+            UnityEngine.Debug.Log(text);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void
+    }
+}
---
+++ Log/Log.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace utility.log {
+    public static class DebugLog {
+
+        [Conditional("UNITY_EDITOR")]
+        public static void Log (object log) {
+            UnityEngine.Debug.Log(log);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void TextLog (string text) {
+            UnityEngine.Debug.Log(text);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void WarningLog (object log) {
+            UnityEngine.Debug.LogWarning(log);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void WarningTextLog (string text) {
+            UnityEngine.Debug.LogWarning(text);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void ErrorLog (object log) {
+            UnityEngine.Debug.LogError(log);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void ErrorTextLog (string text) {
+            UnityEngine.Debug.LogError(text);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void Asset (bool condition, object log) {
+            UnityEngine.Debug.Assert(condition, log);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void TextAsset (bool condition, string text) {
+            UnityEngine.Debug.Assert(condition, text);
+        }
+    }
+}
